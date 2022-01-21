e303798569572e64cb9b31fc637c2172a5cef74b
Kyle W <hello@example.com>
2022-01-20 19:21:02 -0500

    initial

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true, 
+    open: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+}
